e9d859028f76bdd77eb9f40a041748f0f3ce2738
MANLINDAS <159142596+MANLINDAS@users.noreply.github.com>
2024-10-10 08:14:33 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,81 @@
+criaCartao(
+    'Matemática', 
+    'Qual a formula para calcular o volume de um cone?' , 
+    'area da base x altura / 3'
+  )
+  criaCartao( 
+    'Matemática' , 
+    'Qual é a fórmula da base de um triângulo?' ,
+   'base x altura/2'
+  )
+  criaCartao(
+    'História',
+     'O que é revolução industrial?', 
+    'Período de grandes mudanças econômicas e sociais, marcado pela mecanização da produçao' ,)
+  criaCartao(
+    'Quimica' ,
+    ' Quaal é a fórmula química do gás carbônico?',
+    'CO₂')
+    criaCartao(
+      'História' ,
+      'Em que ano ocorreu a Proclamação da República no Brasil?' ,
+      '1889')
+      criaCartao( 
+      'Fisica' ,
+      'Qual o valor aproximado da aceleração da gravidade da Terra?',
+      '98 m/s2', )
+  criaCartao(
+    'fisica' ,
+    ' O que é a Lei da Inércia?',
+    ' Um corpo em repouso permanece em repouso e um corpo em movimento permanece em movimento a menos que uma força externa atue sobre ele.')
+  criaCartao(
+      'Matemática' ,
+      'Qual a formula da PG',
+      'an = a1 . q(n-1)'), 
+  criaCartao(
+        'Quimica',
+        'O que é um átomo isótopo?',
+        ' Átomos de um mesmo elemento com números diferentes de nêutrons.')
+  criaCartao(
+        'Biologia',
+        'O que é a mitose?',
+        'Processo de divisão celular que resulta em duas células-filhas idênticas.')
+  criaCartao(
+    'Quimica',
+    'Qual é o principal gás responsável pelo efeito estufa?',
+    'Dióxido de carbono (CO₂).')
+  criaCartao(
+    'Fisica', 
+    'Qual a  fórmula da velocidade média?',
+    'v=Δs/Δt')
+  criaCartao(
+    'Matemática',
+    'Qual a formula da equação do segundo grau?' ,
+    ' ax2+bx+c=0')
+  criaCartao(
+    'Fisica',
+    'Qual a formula da energia Cinetica?',
+    'Ec=12mv2E')
+  criaCartao(
+    'O que é um angulo reto?',
+    'um angulo de 90 graus')
+  criaCartao(
+    'Matemática',
+    'Qual é a fórmula da função quadrática?',
+    'f(x)=ax2+bx+c.')
+  criaCartao(
+    'Matemática',
+    'Qual é a fórmula da soma dos ângulos internos de um polígono?',
+    '(n-2)x180∘(n - 2)')
+  criaCartao(
+    'Fisica',
+    'O que é a Lei de Lenz?',
+    'Lei que afirma que a direção da corrente induzida em um circuito é tal que cria um campo magnético que se opõe à variação do fluxo magnético que a gerou.')
+  criaCartao(
+    'Matemática',
+    'Qual é a fórmula da circunferência de um círculo?',
+    'C=2πrC')
+  criaCartao(
+    'História',
+    'Quem foi o imperador romano que legalizou o cristianismo?',
+    'Constantino, o Grande.')
